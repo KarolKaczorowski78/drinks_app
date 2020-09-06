68c69acbf3733a5911938bf20024f24b2f2a4da0
Karol <KarolKaczorowski@interia.pl>
2020-09-06 18:23:14 +0200

    Updates

---
+++ precache-manifest.e1b127d9fcd69892955d1cccbd9c4ad3.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "254a1034d39e69fc40ac03a4ee76afa8",
+    "url": "/drinks_app/index.html"
+  },
+  {
+    "revision": "eddc1f9f8c309f37011e",
+    "url": "/drinks_app/static/css/main.3554d63a.chunk.css"
+  },
+  {
+    "revision": "082c075b33584eac7004",
+    "url": "/drinks_app/static/js/2.431147af.chunk.js"
+  },
+  {
+    "revision": "3b360bdbbf2b060d70fbfa785d0a310b",
+    "url": "/drinks_app/static/js/2.431147af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eddc1f9f8c309f37011e",
+    "url": "/drinks_app/static/js/main.cc330682.chunk.js"
+  },
+  {
+    "revision": "f2ed22ed588311e1cc44",
+    "url": "/drinks_app/static/js/runtime-main.397397ce.js"
+  },
+  {
+    "revision": "ec303732d6b734b29fcbfb6371152cb7",
+    "url": "/drinks_app/static/media/AboutImg.ec303732.jpg"
+  },
+  {
+    "revision": "adc071b7785b7158368c676ebd8258af",
+    "url": "/drinks_app/static/media/AlcoholicImg.adc071b7.jpg"
+  },
+  {
+    "revision": "f108d5024dcc965d008f1eb9834690fe",
+    "url": "/drinks_app/static/media/CategoryImg.f108d502.jpg"
+  },
+  {
+    "revision": "27eca13790961351b5e4476e0091e900",
+    "url": "/drinks_app/static/media/ContactImg.27eca137.jpg"
+  },
+  {
+    "revision": "0b64fb43e9c3ca5d82d96156a40a2da8",
+    "url": "/drinks_app/static/media/GlassImg.0b64fb43.jpg"
+  },
+  {
+    "revision": "9aa7acea438385db81a516824f3b83ca",
+    "url": "/drinks_app/static/media/IngredientImg.9aa7acea.jpg"
+  },
+  {
+    "revision": "f49e0959e2095d3b16d196d304c3e6b5",
+    "url": "/drinks_app/static/media/IntroductionImg.f49e0959.jpg"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/drinks_app/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/drinks_app/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/drinks_app/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/drinks_app/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "a5b1d2241ef2aca3aadfa9f52c2e3cab",
+    "url": "/drinks_app/static/media/fa-brands-400.a5b1d224.svg"
+  },
+  {
+    "revision": "1ef86349506d95af50c9ddc67efcb56a",
+    "url": "/drinks_app/static/media/fa-regular-400.1ef86349.svg"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/drinks_app/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/drinks_app/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/drinks_app/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/drinks_app/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "0e19b17d33e4d9cb690576eae576457f",
+    "url": "/drinks_app/static/media/fa-solid-900.0e19b17d.svg"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/drinks_app/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/drinks_app/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/drinks_app/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/drinks_app/static/media/fa-solid-900.f3a7d3b5.woff"
+  }
+]);
